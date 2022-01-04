8bb575420788f312ccade9941766b0a50f583f6a
hamdiZH <hamdi.hijazi2018@gmail.com>
2022-01-04 10:03:16 +0200

    setup layout and environemnt

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+  return (
+    <div>
+      <h1 className="text-6xl">Welcome</h1>
+      {process.env.REACT_APP_GITHUB_TOKEN}
+    </div>
+  );
+}
+
+export default Home;
